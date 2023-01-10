881f9a5b425cd4e7e7086d8fbc5a1b2ca30e63fa
Shuhei Hagiwara <s.hagiwara@student.fontys.nl>
2023-01-10 13:49:30 +0100

    Add scale

--- src/components/rating_post_vote/VoteForm.js
+++ src/components/rating_post_vote/VoteForm.js
@@ -1,4 +1,4 @@
-import "../styles/PlayersList.css"
+import "../styles/VoteForm.css"
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -62,41 +62,99 @@ function VoteForm(props) {
 
     return (
         <form className="form-container"  onSubmit={handleSubmit}>
-            <table>
-                <thead>
-                    <tr>
-                        <th className="player">Player</th>
-                        <th className="position">Position</th>
-                        <th className="rating">Rating</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {props.players.map(player => {
-                    return (
-                        <tr id={player.id}>
-                            <td>{player.firstname} {player.lastname}</td>
-                            <td>{player.position}</td>
-                            <td>
-                                <select onChange={ratingSelected}>
-                                    <option>-</option>
-                                    <option>0</option>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                    <option>6</option>
-                                    <option>7</option>
-                                    <option>8</option>
-                                    <option>9</option>
-                                    <option>10</option>
-                                </select>
-                            </td>
+            <div className="rating-box">
+                <table className="rating">
+                    <thead>
+                        <tr>
+                            <th className="player">Player</th>
+                            <th className="position">Position</th>
+                            <th className="rating">Rating</th>
                         </tr>
-                    )
-                })}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {props.players.map(player => {
+                        return (
+                            <tr id={player.id}>
+                                <td>{player.firstname} {player.lastname}</td>
+                                <td>{player.position}</td>
+                                <td>
+                                    <select onChange={ratingSelected}>
+                                        <option>-</option>
+                                        <option>0</option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                        <option>8</option>
+                                        <option>9</option>
+                                        <option>10</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+                </table>
+                <div className="legend">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Rating</th>
+                                <th>Performance</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>10</td>
+                                <td>Perfect</td>
+                            </tr>
+                            <tr>
+                                <td>9</td>
+                                <td>Outstanding</td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td>Very good</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>Good</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>Satisfactory</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>OK</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Unsatisfactory</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Bad</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Very bad</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Unbelievably bad</td>
+                            </tr>
+                            <tr>
+                                <td>0</td>
+                                <td>Unacceptable behavior</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
             <div>
                 <button className="input-submit">Submit</button>
                 <div className="error">{error}</div>
