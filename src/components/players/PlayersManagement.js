09e96802d8cbba0a17cac68b94951cad08fc6edc
Shuhei Hagiwara <s.hagiwara@student.fontys.nl>
2023-01-14 15:28:58 +0100

    fix: UI UX

--- src/components/players/PlayersManagement.js
+++ src/components/players/PlayersManagement.js
@@ -1,7 +1,7 @@
 import PlayersManagementList from "./PlayersManagementList";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "../styles/PlayersManagement.css"
+import styles from "../styles/PlayersManagement.module.css"
 import { useNavigate } from "react-router-dom";
 
 function PlayersManagement() {
@@ -71,23 +71,23 @@ function PlayersManagement() {
 
     return (
         <div>
-            <div className="button-box">
-                <button className="add-post" onClick={() => navigate("/addplayer")}>
-                    <div className="plus-button">+</div>
+            <div className={styles.buttonbox}>
+                <button className={styles.button} onClick={() => navigate("/addplayer")}>
+                    <div className={styles.plus}>+</div>
                     <div>Add Player</div>
                 </button>
-                <button className="add-post" onClick={() => navigate("/deleteplayer")}>
-                    <div className="plus-button">-</div>
+                <button className={styles.button} onClick={() => navigate("/deleteplayer")}>
+                    <div className={styles.plus}>-</div>
                     <div>Delete Player</div>
                 </button>
             </div>
-            <div className="management-position-title">Goalkeepers</div>
+            <div className={styles.title}>Goalkeepers</div>
             <PlayersManagementList players={GKplayers} />
-            <div className="management-position-title">Defenders</div>
+            <div className={styles.title}>Defenders</div>
             <PlayersManagementList players={DFplayers} />
-            <div className="management-position-title">Midfielders</div>
+            <div className={styles.title}>Midfielders</div>
             <PlayersManagementList players={MFplayers} />
-            <div className="management-position-title">Forwards</div>
+            <div className={styles.title}>Forwards</div>
             <PlayersManagementList players={FWplayers} />
         </div>
     )
--- src/components/players/PlayersManagementList.js
+++ src/components/players/PlayersManagementList.js
@@ -1,4 +1,4 @@
-import "../styles/PlayersManagementList.css"
+import styles from "../styles/PlayersManagementList.module.css"
 import { useEffect } from "react";
 
 function PlayersManagementList(props) {
@@ -8,10 +8,10 @@ function PlayersManagementList(props) {
       }, []);
 
     return (
-        <div className="players">
+        <div>
             {props.players.map(player => {
                 return (
-                    <div className="player" id={player.player.id}>
+                    <div className={styles.player} id={player.player.id}>
                         <td>{player.player.firstname.charAt(0).toUpperCase() + player.player.firstname.slice(1)} {player.player.lastname.charAt(0).toUpperCase() + player.player.lastname.slice(1)}</td>
                     </div>
                 )
