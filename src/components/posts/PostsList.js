53f353338d8013fe225f5451eb120e44e5c8cbd6
Shuhei Hagiwara <s.hagiwara@student.fontys.nl>
2023-01-15 19:35:14 +0100

    fix: bug

--- src/components/posts/PostsList.js
+++ src/components/posts/PostsList.js
@@ -2,14 +2,21 @@ import PostCard from './PostCard';
 import React from "react"
 
 function PostsList(props) {
-
   return (
     <div>
-      {props.posts.sort((a, b) => b.vote - a.vote).map(post => (
+      {props.posts.sort((a, b) => {
+        const voteWeight = 0.4;
+        const dateWeight = 0.6;
+
+        const scoreA = a.vote * voteWeight + new Date(a.date).getTime() * dateWeight;
+        const scoreB = b.vote * voteWeight + new Date(b.date).getTime() * dateWeight;
+
+        return scoreB - scoreA;
+      }).map(post => (
         <PostCard key={post.id} post={post} />
       ))}
     </div>
-  )
+  );
 }
 
 export default PostsList;
